705f9f95f159dd1da5040cabfa7ce0bc6ecc8674
omkarspy <yerunkar@usc.edu>
2015-04-12 19:44:23 -0700

    Stock Search Comments
    Comments added

--- Web Technologies/Stock Search Project/stock_servlet.java
+++ Web Technologies/Stock Search Project/stock_servlet.java
@@ -36,13 +36,10 @@ public class stock_servlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//System.out.println("sdss");
 		String symbol= request.getParameter("symbol");
 		System.out.println(symbol);
 		response.setContentType("application/json");
-		//changed
 		String encodesymb = URLEncoder.encode(symbol,"UTF-8");//symbol; 
-		//encodesymb = encodesymb.replaceAll("'","/'");
 		String url_php = "http://default-environment-qzy28j5khm.elasticbeanstalk.com/?symbol="+encodesymb;
 		URL url= new URL(url_php);
 		URLConnection connection= url.openConnection();
@@ -50,8 +47,6 @@ public class stock_servlet extends HttpServlet {
 		
 	 
 	    BufferedReader stdin = new BufferedReader(new InputStreamReader(url.openStream())); 
-		//InputStreamReader isr= new InputStreamReader(url.openStream());
-		//BufferedReader stdin= new BufferedReader(isr);
 		String input= new String();
 		String xml_content="";
 		while((input=stdin.readLine()) !=null)
@@ -61,20 +56,15 @@ public class stock_servlet extends HttpServlet {
 	
 		System.out.println(xml_content);
 		// Converting XML data to JSON
-		 
-		
-			JSONObject xmlJSONObj;
-			try {
-				xmlJSONObj = XML.toJSONObject(xml_content);
-				PrintWriter writer = response.getWriter(); 
-			    writer.println(xmlJSONObj);
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-					
-		
+		JSONObject xmlJSONObj;
+		try {
+			xmlJSONObj = XML.toJSONObject(xml_content);
+			PrintWriter writer = response.getWriter(); 
+		    writer.println(xmlJSONObj);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
